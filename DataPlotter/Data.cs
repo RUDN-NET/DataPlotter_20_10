92d2bf672f03f5e9b9654e47a1325d1f685753a4
Infarh <shmachilin@yandex.ru>
2020-10-20 09:58:31 +0300

    Метод чтения файла данных по строкам

--- DataPlotter/Data.cs
+++ DataPlotter/Data.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace DataPlotter
@@ -9,9 +10,31 @@ namespace DataPlotter
     {
         public static DataPoint[] ReadDataFromFile(string FileName)
         {
-
+            var points = GetPoints(FileName).ToArray();
 
             return null; // Чтобы компилятор не ругался...
+        }
+
+        private static IEnumerable<DataPoint> GetPoints(string FileName)
+        {
+            using (var file = File.OpenText(FileName))
+            {
+                file.ReadLine();
+
+                while (!file.EndOfStream)
+                {
+                    var line = file.ReadLine();
+
+                    var xy_str = line.Split(';');
+
+                    var x = double.Parse(xy_str[0]);
+                    var y = double.Parse(xy_str[1]);
+
+                    var point = new DataPoint(x, y);
+
+                    yield return point;
+                }
+            }
         }
 
         public static void WriteDataToFile(DataPoint[] Points, string FileName)
