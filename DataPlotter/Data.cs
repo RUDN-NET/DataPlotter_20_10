d0617783cf35f0a5c3bf28b2e544fc73fff9f174
Infarh <shmachilin@yandex.ru>
2020-10-20 09:47:00 +0300

    Методы работы с данными перенесены в отдельный класс Data

---
+++ DataPlotter/Data.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DataPlotter
+{
+    internal static class Data
+    {
+        public static void WriteDataToFile(DataPoint[] Points, string FileName)
+        {
+            using (var file = File.CreateText(FileName))
+            {
+                file.WriteLine("x;f(x)");
+
+                for (var i = 0; i < Points.Length; i++)
+                {
+                    file.WriteLine("{0};{1}", Points[i].X, Points[i].Y);
+                }
+            }
+        }
+
+        public static double Sinc(double x)
+        {
+            if (x == 0) return 1;
+            return Math.Sin(x) / x;
+        }
+
+        public static DataPoint[] CreateData(
+            Func<double, double> f,
+            double Xmin, double Xmax,
+            double dx
+        )
+        {
+            var points_count = (int)((Xmax - Xmin) / dx) + 1;
+
+            var points = new DataPoint[points_count];
+
+            for (var i = 0; i < points_count; i++)
+            {
+                var x = i * dx + Xmin;
+                var y = f(x);
+
+                points[i] = new DataPoint(x, y);
+            }
+
+            return points;
+        }
+    }
+}
--- DataPlotter/MainForm.cs
+++ DataPlotter/MainForm.cs
@@ -26,49 +26,11 @@ namespace DataPlotter
             var selected_file = save_file_dialog.FileName;
 
             
-            var data = CreateData(x => Sinc(2 * Math.PI * x), -5, 5, 0.1);
+            var data = Data.CreateData(x => Data.Sinc(2 * Math.PI * x), -5, 5, 0.1);
 
-            WriteDataToFile(data, selected_file);
+            Data.WriteDataToFile(data, selected_file);
         }
 
-        private static void WriteDataToFile(DataPoint[] Points, string FileName)
-        {
-            using (var file = File.CreateText(FileName))
-            {
-                file.WriteLine("x;f(x)");
-
-                for (var i = 0; i < Points.Length; i++)
-                {
-                    file.WriteLine("{0};{1}", Points[i].X, Points[i].Y);
-                }
-            }
-        }
-
-        private static double Sinc(double x)
-        {
-            if (x == 0) return 1;
-            return Math.Sin(x) / x;
-        }
-
-        private static DataPoint[] CreateData(
-            Func<double, double> f,
-            double Xmin, double Xmax,
-            double dx
-            )
-        {
-            var points_count = (int)((Xmax - Xmin) / dx) + 1;
-
-            var points = new DataPoint[points_count];
-
-            for(var i = 0; i < points_count; i++)
-            {
-                var x = i * dx + Xmin;
-                var y = f(x);
-
-                points[i] = new DataPoint(x, y);
-            }
-
-            return points;
-        }
+        
     }
 }
